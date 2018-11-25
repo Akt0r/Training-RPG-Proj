721af3b943e270b8562b0701a3d9364db6d65770
Anton Nikolaev <anton_akter@mail.ru>
2018-11-25 13:29:48 +0300

    Introduced Game Item class and some support for it

---
+++ TrainingRPG/Engine/Factories/ItemFactory.cs
@@ -0,0 +1,29 @@
+﻿using Engine.Models;
+using System.Collections.Generic;
+
+namespace Engine.Factories
+{
+    internal static class ItemFactory
+    {
+        private static Dictionary<int, GameItem> _standartGameItems;
+
+        static ItemFactory()
+        {
+            _standartGameItems = new Dictionary<int, GameItem>
+            {
+                { 1001, new Weapon(1001, "Pointy Stick", 1, 1, 2) },
+                { 1002, new Weapon(1002, "Rusty Sword", 5, 1, 3) }
+            };
+        }
+
+        public static GameItem CreateGameItem(int itemTypeId)
+        {
+            GameItem result;
+            if (_standartGameItems.TryGetValue(itemTypeId, out result))
+            {
+                return result.Clone();
+            }
+            return null;
+        }
+    }
+}
---
+++ TrainingRPG/Engine/Models/Weapon.cs
@@ -0,0 +1,22 @@
+﻿namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+
+        public Weapon(int itemTypeId, string name, int price, int minDamage, int maxDamage) : base(itemTypeId, name, price)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+        }
+
+        public override GameItem Clone()
+        {
+            var clone = (Weapon)base.Clone();
+            clone.MinDamage = MinDamage;
+            clone.MaxDamage = MaxDamage;
+            return clone;
+        }
+    }
+}
